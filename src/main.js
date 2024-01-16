18b875fbee2872045b64e218f0a4bb638111dd93
AinaLuc <luaina.mada@gmail.com>
2024-01-16 22:08:05 +0300

    error

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
-// src/main.js
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 const app = createApp(App);
+
+// Install BootstrapVue
+app.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+app.use(IconsPlugin);
 
 app.use(router);
 
 app.mount('#app');
+
